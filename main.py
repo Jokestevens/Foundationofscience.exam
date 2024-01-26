34d78aa28df69d80791353d694e92e3967bf9a1c
Piomoki Stevens Onajokeoghene <stevensjoke4@gmail.com>
2024-01-26 10:40:02 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookdb.db'
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    publication_year = db.Column(db.Integer, nullable=False)
+
+def create_db():
+    with app.app_context():
+        db.create_all()
+
+@app.route('/books')
+def list_books():
+    books = Book.query.all()
+    return render_template('list_books.html', books=books)
+
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        publication_year = request.form['publication_year']
+
+        new_book = Book(title=title, author=author, publication_year=publication_year)
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('list_books'))
+
+    return render_template('add_book.html', title='Add a Book')
+
+if __name__ == '__main__':
+    create_db()
+    app.run(port=5002, debug=True)
 
 
 
